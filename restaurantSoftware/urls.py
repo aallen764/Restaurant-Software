b89aa60b6e88f0671f665893ce9c79669dc694b7
Daniel Benson <dbensonposts@gmail.com>
2025-03-13 17:08:03 -0400

    added 'SignUp' to the navbar

--- restaurantSoftware/urls.py
+++ restaurantSoftware/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('login.urls')),
     path('', include('home.urls')),
+    path('', include('login.urls')),
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
