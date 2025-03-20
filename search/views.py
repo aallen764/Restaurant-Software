dac5228a68b49db62d7270090e5e7bc04103ba49
aallen764 <aubr3yreyne@gmail.com>
2025-03-20 17:23:49 -0400

    searching only runs when search page is active

--- search/views.py
+++ search/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import requests
+from django.contrib.auth.decorators import login_required
+from .models import user_Profile
 
 # Create your views here.
 def index(request):
     template_data = {}
     template_data['title'] = 'Restaurant Search'
+
+    # pulling user to get their zipcode
+    user = request.user
+
+    zip_code = '33801'
+    
+    city = get_city_from_zip(zip_code)
+    print(city)
+
+    businesses = findRestaurants(city)
+    for business in businesses:
+        print(business['name'])
+
     return render(request, 'search/index.html', {'template_data': template_data})
 
 
@@ -19,9 +34,6 @@ def get_city_from_zip(zip_code):
     data = response.json()
     return data.get('city', 'City not found')
 
-zip_code = '33801'
-city = get_city_from_zip(zip_code)
-print(city)
 
 def findRestaurants(city):
     API_KEY = '8IsAViZ9EV4sgczPyama_sNAMtRagiWIQk1nU6QdQA6QJ5iR3L9Exd-fBXkOKVQHQW93gySAvGrNyzTBz0C7UA0lX1Z__z_PLLkVug3hb2vSrH4VguGtHhC9T1HTZ3Yx'
@@ -44,7 +56,3 @@ def findRestaurants(city):
         print(f"Error: {response.status_code} - {response.text}")
         return None
     
-businesses = findRestaurants(city)
-for business in businesses:
-    print(business['name'])
-
