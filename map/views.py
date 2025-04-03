175de347528d24e4bfad81ec8d2cfbd203842766
Daniel Benson <dbensonposts@gmail.com>
2025-04-03 16:36:27 -0400

    Made search and map page conditional, allowing only logged-in users to access page, to handle exceptions with API calls that need a user's information.

--- map/views.py
+++ map/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.views import View
 
 
 # Create your views here.
 class MapView(View):
+    
     template_name = "map/index.html"
 
     def get(self, request):
-        key = 'AIzaSyAREHr_JNo0KmsVoRgcKSU9t_vqk1mz0No'
-        context = { 
-            "key":key
-        }
-        return render(request, self.template_name, context)
+        if request.user.is_authenticated:
+            key = 'AIzaSyAREHr_JNo0KmsVoRgcKSU9t_vqk1mz0No'
+            context = { 
+                "key":key
+            }
+            return render(request, self.template_name, context)
+        else:
+            return redirect('/')
--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 import requests
@@ -7,26 +7,29 @@ from signUp.models import user_Profile
 
 # Create your views here.
 def index(request):
-    template_data = {}
-    template_data['title'] = 'Restaurant Search'
+    if request.user.is_authenticated:
+        template_data = {}
+        template_data['title'] = 'Restaurant Search'
 
-    # pulling user to get their zipcode
-    user = request.user
+        # pulling user to get their zipcode
+        user = request.user
 
-    try:
-        user_profile = user.user_profile  # Accessing the user_profile linked to the user
-        zip_code = user_profile.zip_code
-    except user_Profile.DoesNotExist:
-        return HttpResponse("User profile not found.", status=404)
-    
-    city = get_city_from_zip(zip_code)
-    print(city)
+        try:
+            user_profile = user.user_profile  # Accessing the user_profile linked to the user
+            zip_code = user_profile.zip_code
+        except user_Profile.DoesNotExist:
+            return HttpResponse("User profile not found.", status=404)
+        
+        city = get_city_from_zip(zip_code)
+        print(city)
 
-    businesses = findRestaurants(city)
-    for business in businesses:
-        print(business['name'])
+        businesses = findRestaurants(city)
+        for business in businesses:
+            print(business['name'])
 
-    return render(request, 'search/index.html', {'template_data': template_data})
+        return render(request, 'search/index.html', {'template_data': template_data})
+    else:
+        return redirect('/')
 
 
 
