26efefae59d7842c80fb959b51430534bf314b28
Gianna Smith <gasmith2107@gmail.com>
2025-04-07 20:14:37 -0400

    Gianna's Test Cases

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from unittest import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class MapUrlTest(SimpleTestCase):
+    def testMapUrl(self):
+        response = self.client.get(reverse('map.index'))
+
+        #This checks if when the page is ran that it returns a 200 stustua code which stands for 'OK'
+        self.assertEqual(response.status_code, 200)
+
+        #This checks if the page contains an div with the ID map which indicates that the map has loaded 
+        self.assertContains(response, 'id="map"')
--- signUp/tests.py
+++ signUp/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from unittest import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import user_Profile
+
+USER_MODEL = get_user_model()
 
 # Create your tests here.
+class SignUpClientTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('signUp')
+
+    def testSignUp(self):
+        data = {
+            'username': 'fake_user',
+            'password': 'fakebaddie',
+            'email_address': 'fakeuser@gmail.com',  
+            'phone_number': '8764649107',
+            'zip_code': '10013'
+        }
+
+        response = self.client.post(self.url, data)
+
+        person = USER_MODEL.objects.get(username='fake_user')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(USER_MODEL.objects.filter(username='fake_user').exists())
+
+
+
+
