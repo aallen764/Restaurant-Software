81017a9e32a6e1ec3dd16c10b647461431c9ba5c
4C4F4C <matthew@matthewblankenship.com>
2025-04-08 01:12:43 -0400

    Test Cases added to Home and About

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class HomePageTests(TestCase):
+    def setUp(self):
+        # Create a test client to simulate a browser
+        self.client = Client()
+
+    def test_home_index_view_status_code(self):
+        # Check that the homepage responds with HTTP 200 (OK)
+        response = self.client.get(reverse('home.index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_index_template_used(self):
+        # Check that the homepage uses the correct template
+        response = self.client.get(reverse('home.index'))
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_home_index_context_data(self):
+        # Check that the context contains the correct title
+        response = self.client.get(reverse('home.index'))
+        self.assertIn('template_data', response.context)
+        self.assertEqual(response.context['template_data']['title'], 'BiteFinder')
